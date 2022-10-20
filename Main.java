2a4e54f259ae8359a3ccee32d414fa69b25a8afe
RMIvanov18 <RMIvanov18@codingburgas.bg>
2022-10-20 14:00:49 +0300

    commit

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
-public class Main {
-    String languages;
-    Main(String lang) {
-        languages = lang;
-        System.out.println(languages + " Programing language");
+abstract class Fruits
+{
+    abstract void taste();
+}
+class Apple extends Fruits {
+    void taste() {
+        System.out.println("Sweet taste");
     }
+}
+public class Main {
+
     public static void main(String[] args) {
-        Main obj = new Main("Java");
-        Main obj2 = new Main("Python");
-        Main obj3 = new Main("C");
+        Apple a = new Apple();
+        a.taste();
     }
 }
